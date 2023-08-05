af5e46ff7efcd411e3b38ce168a011015fef343c
mark47B <windachae47@gmail.com>
2023-08-06 00:09:38 +0300

    Make migrations with alembic

--- app/config.py
+++ app/config.py
@@ -6,9 +6,12 @@ class Settings(BaseSettings):
     PRJ_VERSION: str
     PRJ_DEBUG_ENVIRONMENT: bool
 
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
     POSTGRES_DB_NAME: str
     POSTGRES_PASSWORD: SecretStr
     POSTGRES_USER: str
+    POSTGRES_CONNECTION_STR: str
 
     REDIS_HOSTS: str
     REDIS_PORT: str
